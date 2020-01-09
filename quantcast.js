e42bc9089e47f9c6c5eebd2af3e7608918d0c9d2
Chris Cai <christopher@stanza.co>
2020-01-09 01:49:21 -0800

    working?????

--- quantcast.js
+++ quantcast.js
@@ -56,8 +56,8 @@ let match = (pool, fields, input, map) => {
       for (let i = 0; i < pool.length; i++) {
         let arr1 = fields.children[entry[0]].valMap[pool[i]].slice()
         let arr2 = entry[1]
-        console.log(arr1)
-        console.log(arr2)
+        // console.log(arr1)
+        // console.log(arr2)
         if (!arr2.every(val => arr1.includes(val))) {
           pool.splice(i, 1)
           i--
@@ -100,41 +100,50 @@ storage.prototype.delete = function(input) {
 }
 
 let s = new storage()
-s.add({"id":1,"last":"Doe","first":"John","location":{"city":"Oakland","state":"CA","postalCode":"94607"},"active":true})
-s.add({"id":2,"last":"Doe","first":"Jane","location":{"city":"San Francisco","state":"CA","postalCode":"94105"},"active":true})
-s.add({"id":3,"last":"Black","first":"Jim","location":{"city":"Spokane","state":"WA","postalCode":"99207"},"active":true})
-s.add({"id":4,"last":"Frost","first":"Jack","location":{"city":"Seattle","state":"WA","postalCode":"98204"},"active":false})
-console.log(s.data)
-console.log(s.fields)
-console.log('ID', s.fields.children['id'])
-console.log('LOCATION', s.fields.children['location'])
-console.log('CITY', s.fields.children['location'].children['city'])
-console.log('STATE', s.fields.children['location'].children['state'])
-console.log('POSTAL', s.fields.children['location'].children['postalCode'])
-s.get({
-  "location": {
-    "state": "WA"
-  },
-  "active": true
-})
+// s.add({"id":1,"last":"Doe","first":"John","location":{"city":"Oakland","state":"CA","postalCode":"94607"},"active":true})
+// s.add({"id":2,"last":"Doe","first":"Jane","location":{"city":"San Francisco","state":"CA","postalCode":"94105"},"active":true})
+// s.add({"id":3,"last":"Black","first":"Jim","location":{"city":"Spokane","state":"WA","postalCode":"99207"},"active":true})
+// s.add({"id":4,"last":"Frost","first":"Jack","location":{"city":"Seattle","state":"WA","postalCode":"98204"},"active":false})
+// console.log(s.data)
+// console.log(s.fields)
+// console.log('ID', s.fields.children['id'])
+// console.log('LOCATION', s.fields.children['location'])
+// console.log('CITY', s.fields.children['location'].children['city'])
+// console.log('STATE', s.fields.children['location'].children['state'])
+// console.log('POSTAL', s.fields.children['location'].children['postalCode'])
+// s.get({
+//   "location": {
+//     "state": "WA"
+//   },
+//   "active": true
+// })
 
-s.get({
-  "id": 1
-})
+// s.get({
+//   "id": 1
+// })
 
+// s.delete({
+//   "id": 1
+// })
+
+// console.log(s.data)
+
+// s.get({
+//   "id": 1
+// })
+
+s.add({"type":"list","list":[2,3,4,5]})
+s.add({"type":"list","list":[3,4,5,6]})
+s.add({"type":"list","list":[4,5,6,7]})
+s.add({"type":"list","list":[5,6,7,8]})
+s.add({"type":"list","list":[6,7,8,9]})
+console.log('TESTING GET')
+s.get({"type":"list","list":[2]})
+s.get({"type":"list","list":[3,4]})
+console.log('TESTING DELETE', s.data)
 s.delete({
-  "id": 1
+  "type":"list",
+  "list":[3,4]
 })
-
 console.log(s.data)
-
-s.get({
-  "id": 1
-})
-
-// s.add({"type":"list","list":[2,3,4,5]})
-// s.add({"type":"list","list":[3,4,5,6]})
-// s.add({"type":"list","list":[4,5,6,7]})
-// s.add({"type":"list","list":[5,6,7,8]})
-// s.add({"type":"list","list":[6,7,8,9]})
-// s.get({"type":"list","list":[2]})
+s.get({"type":"list","list":[3,4]})
